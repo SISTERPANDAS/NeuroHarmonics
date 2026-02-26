82ae94e0d83246b70157e0c5bdf389f0c91577d4
SANIPANDA <panar4022@gmail.com>
2026-02-26 09:28:11 +0530

    ..

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Dashboard from '../pages/Dashboard';
+// import Dashboard from './pages/Dashboard';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <div>React dashboard removed. See Flask app.</div>
   </React.StrictMode>
 );
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,1 @@
+// Moved from client/pages/Dashboard.tsx
